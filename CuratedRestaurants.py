5ed9e6d031adec79f602be525eb2e2cbb9251986
Abhishek Banthia <abanthia@apple.com>
2016-08-28 21:14:31 -0700

    Dayum.

--- CuratedRestaurants.py
+++ CuratedRestaurants.py
@@ -1,25 +1,23 @@
 import requests
 import json
+from Login import Login
 
 class CuratedRestaurants(object):
 	"""docstring for Restaurants"""
 	def __init__(self, authorization):
-		super Restaurants, self).__init__()
+		super (CuratedRestaurants, self).__init__()
 		self.authorization = authorization
 
 
-	def listOfCuratedRestaurantsForCustomerID(customerID):
+	def listOfCuratedRestaurantsForCustomerID(self,customerID):
 		url = "https://api.doordash.com/v1/curated_categories/"
 		querystring = {"consumer_id":customerID,"lat":"37.3896127","lng":"-121.9946316"}
 		headers = {
 	    'accept': 'application/json',
 	    'content-type': 'application/json',
-	     'authorization' : self.authorization
+	     'authorization' : self.authorization}
 		response = requests.request("GET", url, headers=headers, params=querystring)
 		data = json.loads(response.text)
 
 		for suggestion in data[0].get('content'):
 			print suggestion["name"]
-
-
-	listOfCuratedRestaurantsForCustomerID("12607762")
--- Login.py
+++ Login.py
@@ -1,43 +1,26 @@
 import requests
-import json
 
-url = "https://api.doordash.com/v2/auth/token/"
+class Login(object):
+	"""docstring for Login"""
+	def __init__(self):
+		super(Login, self).__init__()
 
-payload = "{\n    \"email\": \"chelsea1712@gmail.com\",\n    \"password\" : \"Abhi1712!\"\n}"
-headers = {
-    'accept': "application/json",
-    'content-type': "application/json"
-    }
 
-response = requests.request("POST", url, data=payload, headers=headers)
+	def authenticate(self,username, password):
+		url = "https://api.doordash.com/v2/auth/token/"
+		payload = "{\"email\": \"chelsea1712@gmail.com\",\"password\" : \"Abhi1712!\"}"
 
-authorizationHeader = []
-authorizationHeader.append("JWT ")
-authorizationHeader.append(response.json().get('token'))
+		headers = {
+		    'accept': "application/json",
+		    'content-type': "application/json"
+		    }
+		
+		response = requests.request("POST", url, data=payload, headers=headers)
 
-url = "https://api.doordash.com/v2/consumer/me/"
-
-headers = {'authorization': ''.join(authorizationHeader)}
-
-print(headers)
-
-response = requests.request("GET", url, headers=headers)
-
-print(response.json().get('first_name') + " is logged in")
-print(response.json().get('default_address').get('printable_address'))
-
-url = "https://api.doordash.com/v1/consumer_search/"
-
-querystring = {"get_suggestions":"1"}
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-url = "https://api.doordash.com/v1/curated_categories/"
-
-querystring = {"consumer_id":"12607762","lat":"37.3896127","lng":"-121.9946316"}
-
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-data = json.loads(response.text)
-
-for suggestion in data[0].get('content'):
-	print suggestion["name"]
+		if response.reason == "OK":
+			authorizationHeader = []
+			authorizationHeader.append("JWT ")
+			authorizationHeader.append(response.json().get('token'))
+			return ''.join(authorizationHeader)
+		else:
+			print response.reason
