25c2701a87c00c0cb909730e807f7582ec428267
Abhishek Banthia <abanthia@apple.com>
2016-09-09 15:29:07 -0700

    Test modifications.

--- API/test_suggestions.py
+++ API/test_suggestions.py
@@ -1,13 +1,16 @@
 import unittest
 from User import User
 from Suggestions import Suggestions
+from geopy.geocoders import Nominatim
 
 class test_suggestions(unittest.TestCase):
 	"""docstring for TestSuite"""
 	def setUp(self):
 		userObject = User()
 		authorizationToken = userObject.authenticate("frodo.baggins16@yandex.com", "frodobaggins")
-		self.suggestionObject = Suggestions(authorizationToken)
+		geolocator = Nominatim()
+		currentLocation = geolocator.geocode("650 Almanor Avenue Sunnyvale California")
+		self.suggestionObject = Suggestions(authorizationToken, currentLocation)
 
 	def test_suggestions(self):
 		suggestions = self.suggestionObject.retrieveSuggestions()
